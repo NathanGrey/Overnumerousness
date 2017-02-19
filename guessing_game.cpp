9ebfe0572c43af3e47712ecd968378cf3ca704f0
Nathan Grey <Nathan@NathanGrey.com>
2017-02-18 23:15:49 -0800

    Update guessing_game.cpp

--- guessing_game.cpp
+++ guessing_game.cpp
@@ -1,63 +1,64 @@
 // Starts the preprocessor command including the contents of iostream
-#inclue <iostream>
+#include <iostream>
 
 // This eliminates the need to write std::cout so it can just be cout
 using namespace std;
 
-/* I've looked up and heard void main () is considered in bad practice? 
+/* I've looked up and heard void main () is considered in bad practice?
 Still not completely understanding why though */
-void main ()
+void main()
 {
 	// Delcaration of variables
 	int firstNumber, secondNumber, thirdNumber, average, guess;
 	// Takes a single character as a response
 	char response;
 
+	// Input/Output for each number
+	cout << "Enter the 1st number. \n";
+	cin >> firstNumber;
+
+	cout << "Enter the 2nd number. \n";
+	cin >> secondNumber;
+
+	cout << "Enter the 3rd number. \n";
+	cin >> thirdNumber;
+
+	cout << "Would you like to guess the average? \n";
+	cin >> response;
+
 	// Calcualtes the average
 	average = (firstNumber + secondNumber + thirdNumber) / 3;
-
-    // Input/Output for each number
-	cout << "Enter the 1st Nnmber";
-	cin >> "firstNumber";
-
-	cout << "Enter the 2nd number";
-	cin >> "secondNumber";
-
-	cout << "Enter the 3rd number4";
-	cin >> "thirdNumber";
-
-	cout << "Would you like to guess the average?";
-	cin >> response;
 
 	// If statement for user inputting y
 	if (response == 'y')
 	{
-		cout << "What's your guess?";
-		cin >> "guess";
+		cout << "What's your guess? \n";
+		cin >> guess;
 		// Another if statement within the main one to check if the guess is correct
 		if (guess == average)
 		{
-			cout << "That's mathematical! You're correct!";
+			cout << "That's mathematical! You're correct! \n";
 		}
 		// Else statement which give the correct answer if user's guess is incorrect
 		else
 		{
-			cout << "Actually it's" << average;
+			cout << "Actually it's" << " " << average << "." << "\n";
 		}
 	}
 	// Else if statement for if the user inputs n
 	else if (response == 'n')
 	{
-		cout << "The average is" << average;
+		cout << "The average is \n" << average;
 	}
 	else
 	{
-		cout << "That's not y or n, but the average is actually" << average;
+		cout << "That's not y or n, but the average is actually" << " " << average << "." << "\n";
 	}
 	// Ends the program once the game is over
-	return 0;
+	system ("pause");
 }
 
-/* Bug notes
+/* Bug Notes
 - Accidentally wrote comments wrong which commented things out
-/*
+- Put calculations for numbers before numbers were used which caused the numbers to not initialize
+*/
